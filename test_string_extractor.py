3dad059fa26873b747af64a4c4c8d9d48f060111
shanesully <shanesully01@gmail.com>
2015-10-16 15:56:38 +0100

    Preliminary string_extractor test

---
+++ test_string_extractor.py
@@ -0,0 +1,13 @@
+import string_extractor
+import unittest
+
+class test_string_extractor(unittest.TestCase):
+
+    def setUp(self):
+        pass 
+    
+    def test_extract_strings(self):
+        src_text = ""
+        expected_strings = ""
+
+        self.assertEqual(expected_strings, src_text)
